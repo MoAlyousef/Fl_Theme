0cf1e003d7838fe0e10c7e4bba29673e91df68c9
Mohammed Alyousef <may642_2000@hotmail.com>
2022-04-20 15:13:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-#include "Fl_Theme/Fl_Theme.hpp"
-#include "Fl_Theme/colors/html.hpp"
-
-extern int FLAT_BORDER_RADIUS; // for if we need to change the radius
-
 #include <FL/Fl.H>
 #include <FL/Enumerations.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Input.H>
 #include <FL/Fl_Menu_Bar.H>
 #include <FL/Fl_Window.H>
+
+#include "Fl_Theme/Fl_Theme.hpp"
+#include "Fl_Theme/colors/html.hpp"
+
+extern int FLAT_BORDER_RADIUS; // for if we need to change the radius
 
 int main() {
     WidgetTheme::apply(ThemeType::Dark);
@@ -17,12 +17,11 @@ int main() {
     menu->add("File/New\t");
     menu->add("File/Open\t");
     auto inp = new Fl_Input(100, 100, 200, 30, "");
-    auto box = new Fl_Button(160, 200, 80, 30, "Box");
-    box->labelcolor(White);
-    box->color(DarkRed);
-    box->selection_color(DarkRed);
-    box->callback([](Fl_Widget* w) { w->parent()->redraw(); });
-    box->clear_visible_focus();
+    auto btn = new Fl_Button(160, 200, 80, 30, "Click");
+    btn->labelcolor(White);
+    btn->color(DarkRed);
+    btn->selection_color(DarkRed);
+    btn->clear_visible_focus();
     win->end();
     win->show();
     return Fl::run();
