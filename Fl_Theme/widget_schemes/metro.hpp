f927a9ce9a7473207ac48790c19703c8ebb70ef6
Mohammed Alyousef <may642_2000@hotmail.com>
2022-04-23 16:49:24 +0300

    metro fixes

--- Fl_Theme/widget_schemes/metro.hpp
+++ Fl_Theme/widget_schemes/metro.hpp
@@ -7,7 +7,7 @@ inline void metro_upbox(int x, int y, int w, int h, Fl_Color col) {
         "<rect x='2' y='2' width='%d' height='%d' rx='0' stroke-width='1' stroke='rgb(0,0,0)' />"
         "<rect x='2' y='2' width='%d' height='%d' rx='0' fill='rgb(245,245,245)' />"
         "<rect x='2' y='%d' width='%d' height='%d' rx='0' fill='rgb(230,230,230)' />"
-        "</svg>", w, h, w - 4, h -4, w - 4, (h - 4)/2, (h - 4)/2, w -4 , h / 2);
+        "</svg>", w, h, w - 6, h -4, w - 6, (h - 4)/2, (h - 4)/2, w -6 , h / 2);
     detail::draw_image(x, y, w, h, svg);
     free(svg);
 }
@@ -18,7 +18,7 @@ inline void metro_downbox(int x, int y, int w, int h, Fl_Color col) {
         "<rect x='2' y='2' width='%d' height='%d' rx='0' stroke-width='1' stroke='rgb(0,0,0)' />"
         "<rect x='2' y='2' width='%d' height='%d' rx='0' fill='rgb(230,230,230)' />"
         "<rect x='2' y='%d' width='%d' height='%d' rx='0' fill='rgb(245,245,245)' />"
-        "</svg>", w, h, w - 4, h -4, w - 4, (h - 4)/2, (h - 4)/2, w -4 , h / 2);
+        "</svg>", w, h, w - 6, h -4, w - 6, (h - 4)/2, (h - 4)/2, w -6 , h / 2);
     detail::draw_image(x, y, w, h, svg);
     free(svg);
 }
@@ -30,7 +30,7 @@ inline void metro_active_upbox(int x, int y, int w, int h, Fl_Color col) {
         "<rect x='2' y='2' width='%d' height='%d' rx='0' stroke-width='2' stroke='rgb(69, 179, 234)' />"
         "<rect x='2' y='2' width='%d' height='%d' rx='0' fill='rgb(245,245,245)' />"
         "<rect x='2' y='%d' width='%d' height='%d' rx='0' fill='rgb(230,230,230)' />"
-        "</svg>", w, h, w - 4, h -4, w - 4, (h - 4)/2, (h - 4)/2, w -4 , h / 2);
+        "</svg>", w, h, w - 6, h - 4, w - 6, (h - 4)/2, (h - 4)/2, w - 6 , h / 2);
     detail::draw_image(x, y, w, h, svg);
     free(svg);
 }
@@ -41,7 +41,7 @@ inline void metro_active_downbox(int x, int y, int w, int h, Fl_Color col) {
         "<rect x='2' y='2' width='%d' height='%d' rx='0' stroke-width='2' stroke='rgb(69, 179, 234)' />"
         "<rect x='2' y='2' width='%d' height='%d' rx='0' fill='rgb(230,230,230)' />"
         "<rect x='2' y='%d' width='%d' height='%d' rx='0' fill='rgb(245,245,245)' />"
-        "</svg>", w, h, w - 4, h -4, w - 4, (h - 4)/2, (h - 4)/2, w -4 , h / 2);
+        "</svg>", w, h, w - 6, h - 4, w - 6, (h - 4)/2, (h - 4)/2, w - 6 , h / 2);
     detail::draw_image(x, y, w, h, svg);
     free(svg);
 }
